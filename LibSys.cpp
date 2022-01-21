96846d2235b42aa9de2ef3e86732943a55233b70
Streamer0320 <1565948255@qq.com>
2022-01-22 01:32:01 +0800

    test

--- LibSys.cpp
+++ LibSys.cpp
@@ -1,20 +1,33 @@
-﻿// LibSys.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿// @name         LibSys
+// @version      1.0.0
+// @author       streamer0320
+// @description  Library information management system (Object oriented programming C++)
+// @license      MIT License
 
-#include <iostream>
+#include "LibSys.h"
+using namespace std;
 
+void Book::write()
+{
+    cout << "please input 7 string: ";
+    for (int i = 0; i < inf; i++)
+    {
+        cin >> book[i];
+    }
+}
+void Book::display()
+{
+    cout << "7 string such as: ";
+    for (int i = 0; i < inf; i++)
+    {
+        cout << book[i] << " ";
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    Book b;
+    b.write();
+    b.display();
+
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
