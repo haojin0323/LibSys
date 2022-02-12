b374e80d1e1362dcdbd4a70f6040ef2ccb359854
Streamer0320 <1565948255@qq.com>
2022-02-12 17:39:23 +0800

    upload project

--- LibSys.cpp
+++ LibSys.cpp
@@ -1,45 +1,40 @@
-﻿// @name         LibSys
+// @name         LibSys
 // @version      1.0.0
 // @author       streamer0320
 // @description  Library information management system (Object oriented programming C++)
 // @license      MIT License
 
 #include <iostream>
+#include <iomanip>
 #include <string>
-#define inf 7	// 
+#include <vector>
+using namespace std;
 
 class Book
 {
 public:
-    Book() {}
-    void write();
-    void display();
+    Book();
 private:
-    std::string book[inf];
-};
-using namespace std;
 
-void Book::write()
+};
+class User
 {
-    cout << "please input 7 string: ";  // 根据 inf 的数据数量提示输入信息
-    for (int i = 0; i < inf; i++)
-    {
-        cin >> book[i];
-    }
-}
-void Book::display()
+public:
+    User();
+private:
+
+};
+class Admin
 {
-    cout << "7 string such as: ";
-    for (int i = 0; i < inf; i++)
-    {
-        cout << book[i] << " ";
-    }
-}
+public:
+    Admin();
+private:
+
+};
+
+
 int main()
 {
-    Book b;
-    b.write();
-    b.display();
 
     return 0;
 }
